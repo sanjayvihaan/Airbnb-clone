26a3701bf1b65f67ee11622d6a55f56d6da8f282
V Sanjay <sanjayvihaan111@gmail.com>
2021-08-05 12:21:19 +0530

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+    return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is not a real site</p>
+            <p>Its a pretty awesome place to stay</p>
+            <p>Frontline Stays</p>
+            <p> Guest Referrals</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is not a real site</p>
+            <p>Its a pretty awesome place to stay</p>
+            <p>Frontline Stays</p>
+            <p> Guest Referrals</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">Host</h5>
+            <p>Host your home</p>
+            <p>Host an Online Experience</p>
+            <p>Responsible hosting</p>
+            <p>Resource Center</p>
+            <p>Community Center</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">SUPPORT</h5>
+            <p>Help Center</p>
+            <p>Trust & Safety</p>
+            <p>Cancellation options</p>
+            <p>Neighbourhood Support</p>
+            <p>Terms and Conditions</p>
+        </div>
+
+    </div>
+    )
+}
+
+export default Footer
